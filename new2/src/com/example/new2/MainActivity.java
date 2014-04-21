d2018429a28a18387e9ba35ddcc124c33fc76aec
almaz <almaz.yitref@yahoo.com>
2014-04-21 16:00:02 -0700

    Chapter3

---
+++ new2/src/com/example/new2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.new2;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		
+	}
+
+	
+
+}
